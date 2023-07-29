036c9a9453735e3df41fa2b6e6cee620b280149a
NiranjanTek <140894091+NiranjanTek@users.noreply.github.com>
2023-07-29 15:56:40 +0200

    Create Test.cpp

---
+++ Test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cout << "Hey Niranjan!\n";
+    cout << "How are you? \n";
+    cout << "I am fine\n";
+    cout << "How are you?\n";
+    cout << "Fine dude";
+    return 0;
+}
